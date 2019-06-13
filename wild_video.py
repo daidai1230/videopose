f89dc8ac65922ae054f0ff29b36d2fc1fd924932
daidai1230 <dyd23@qq.com>
2019-06-13 23:04:08 +0800

    .

--- wild_video.py
+++ wild_video.py
@@ -1,3 +1,7 @@
+from common.visualization import render_animation
+from data.data_utils import suggest_metadata, suggest_pose_importer
+from common.h36m_dataset import Human36mDataset
+import time
 import numpy as np
 
 from common.arguments import parse_args
@@ -19,8 +23,9 @@ from time import time
 args = parse_args()
 print(args)
 
-import time
 # record time
+
+
 def ckpt_time(ckpt=None):
     if not ckpt:
         return time.time()
@@ -30,15 +35,13 @@ def ckpt_time(ckpt=None):
 
 time0 = ckpt_time()
 print('Loading 3D dataset...')
-dataset_path = 'data/data_3d_' + args.dataset + '.npz' #  dataset 'h36m'
-from common.h36m_dataset import Human36mDataset
-dataset = Human36mDataset(dataset_path) #'data/data_3d_h36m.npz'
+dataset_path = 'data/data_3d_' + args.dataset + '.npz'  # dataset 'h36m'
+dataset = Human36mDataset(dataset_path)  # 'data/data_3d_h36m.npz'
 
 ckpt, time1 = ckpt_time(time0)
 print('load 3D dataset spend {:2f} second'.format(ckpt))
 
 # according to output name,generate some format. we use detectron
-from data.data_utils import suggest_metadata, suggest_pose_importer
 metadata = suggest_metadata('detectron_pt_coco')
 print('Loading 2D detections keypoints ...')
 
@@ -63,23 +66,27 @@ print('load 2D dataset spend {:2f} second'.format(ckpt))
 
 keypoints_symmetry = metadata['keypoints_symmetry']
 kps_left, kps_right = list(keypoints_symmetry[0]), list(keypoints_symmetry[1])
-joints_left, joints_right = list(dataset.skeleton().joints_left()), list(dataset.skeleton().joints_right())
+joints_left, joints_right = list(dataset.skeleton().joints_left()), list(
+    dataset.skeleton().joints_right())
 
 # normlization keypoints
 # 假设use the camera parameter
 cam = dataset.cameras()['S1'][0]
-keypoints[..., :2] = normalize_screen_coordinates(keypoints[..., :2], w=cam['res_w'], h=cam['res_h'])
+keypoints[..., :2] = normalize_screen_coordinates(
+    keypoints[..., :2], w=cam['res_w'], h=cam['res_h'])
 
 filter_widths = [int(x) for x in args.architecture.split(',')]
 model_pos = TemporalModel(17, input_num, 17, filter_widths, causal=args.causal, dropout=args.dropout, channels=args.channels,
-                            dense=args.dense)
+                          dense=args.dense)
 if torch.cuda.is_available():
     model_pos = model_pos.cuda()
 
 # load trained model
-chk_filename = os.path.join(args.checkpoint, args.resume if args.resume else args.evaluate)
+chk_filename = os.path.join(
+    args.checkpoint, args.resume if args.resume else args.evaluate)
 print('Loading checkpoint', chk_filename)
-checkpoint = torch.load(chk_filename, map_location=lambda storage, loc: storage)# 把loc映射到storage
+checkpoint = torch.load(
+    chk_filename, map_location=lambda storage, loc: storage)  # 把loc映射到storage
 model_pos.load_state_dict(checkpoint['model_pos'])
 
 ckpt, time3 = ckpt_time(time2)
@@ -87,9 +94,8 @@ print('load 3D pose spend {:2f} second'.format(ckpt))
 
 #  Receptive field: 243 frames for args.arc [3, 3, 3, 3, 3]
 receptive_field = model_pos.receptive_field()
-pad = (receptive_field - 1) // 2 # Padding on each side
+pad = (receptive_field - 1) // 2  # Padding on each side
 causal_shift = 0
-
 
 
 def evaluate(test_generator, action=None, return_predictions=False):
@@ -104,13 +110,14 @@ def evaluate(test_generator, action=None, return_predictions=False):
             # Positional model
             predicted_3d_pos = model_pos(inputs_2d)
 
-
             # Test-time augmentation (if enabled)
             if test_generator.augment_enabled():
                 # Undo flipping and take average with non-flipped version
                 predicted_3d_pos[1, :, :, 0] *= -1
-                predicted_3d_pos[1, :, joints_left + joints_right] = predicted_3d_pos[1, :, joints_right + joints_left]
-                predicted_3d_pos = torch.mean(predicted_3d_pos, dim=0, keepdim=True)
+                predicted_3d_pos[1, :, joints_left +
+                                 joints_right] = predicted_3d_pos[1, :, joints_right + joints_left]
+                predicted_3d_pos = torch.mean(
+                    predicted_3d_pos, dim=0, keepdim=True)
 
             if return_predictions:
                 return predicted_3d_pos.squeeze(0).cpu().numpy()
@@ -121,8 +128,8 @@ print('Rendering...')
 input_keypoints = keypoints.copy()
 
 gen = UnchunkedGenerator(None, None, [input_keypoints],
-                            pad=pad, causal_shift=causal_shift, augment=args.test_time_augmentation,
-                            kps_left=kps_left, kps_right=kps_right, joints_left=joints_left, joints_right=joints_right)
+                         pad=pad, causal_shift=causal_shift, augment=args.test_time_augmentation,
+                         kps_left=kps_left, kps_right=kps_right, joints_left=joints_left, joints_right=joints_right)
 prediction = evaluate(gen, return_predictions=True)
 
 
@@ -142,21 +149,24 @@ anim_output = {'Reconstruction': prediction}
 ckpt, time4 = ckpt_time(time3)
 print('restruction 3D pose spends {:2f} second'.format(ckpt))
 
-input_keypoints = image_coordinates(input_keypoints[..., :2], w=cam['res_w'], h=cam['res_h'])
+input_keypoints = image_coordinates(
+    input_keypoints[..., :2], w=cam['res_w'], h=cam['res_h'])
 
 # set default fps = 25 dataset.fps()  = 25
 #  import ipdb;ipdb.set_trace()
 
-from common.visualization import render_animation
 if args.input_npz:
-    viz_output = args.input_npz.split('.')[0] + '_' +args.checkpoint.split('/')[-1] + time.strftime('_%Y_%m_%d',time.localtime(time.time()))
+    viz_output = args.input_npz.split('.')[0] + '_' + args.checkpoint.split(
+        '/')[-1] + time.strftime('_%Y_%m_%d.', time.localtime(time.time())) + args.viz_output
 else:
     viz_output = args.viz_output
 render_animation(input_keypoints, anim_output,
-                    dataset.skeleton(), 25, args.viz_bitrate, cam['azimuth'], viz_output,
-                    limit=args.viz_limit, downsample=args.viz_downsample, size=args.viz_size,
-                    input_video_path=args.viz_video, viewport=(cam['res_w'], cam['res_h']),
-                    input_video_skip=args.viz_skip)
+                 dataset.skeleton(
+                 ), 25, args.viz_bitrate, cam['azimuth'], viz_output,
+                 limit=args.viz_limit, downsample=args.viz_downsample, size=args.viz_size,
+                 input_video_path=args.viz_video, viewport=(
+                     cam['res_w'], cam['res_h']),
+                 input_video_skip=args.viz_skip)
 
 
 ckpt, time5 = ckpt_time(time4)
